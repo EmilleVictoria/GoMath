bed5a49efcecdb1b39bd2018b2e361198e61509d
EmilleCorreia4 <evbc@discente.ifpe.edu.br>
2023-01-21 11:14:42 -0300

    Adicionando admin + estilização

--- authentication.js
+++ authentication.js
@@ -1,22 +1,42 @@
 
+
 //funcao de login
 function login() {
+  
+    let email = document.getElementById("inputEmail").value;
+    let senha = document.getElementById("inputSenha").value;
+    let admin = null;
+    if (email == "gomathadmin@gmail.com" && senha == "aprovadas"){
+       admin = true;
+    }else{
+     admin = false;
+    }
+ 
+ 
     firebase.auth().signInWithEmailAndPassword(
         //pega as informacoes do formulario e armazenam
         document.getElementById("inputEmail").value, document.getElementById("inputSenha").value
     ).then(response => {
          console.log("LogIn efetuado com sucesso!");
-         
-         //manda para Dashboard
-         window.location.href='/Dashboard/dashboard.html';
-       
-
+         if (admin == false){
+            //manda para Dashboard
+            window.location.href='/Dashboard/dashboard.html';
+         }else{
+            window.location.href='/criarSimulado/index.html';
+         }
+ 
+ 
     }).catch(error => {
         alert(getErrorMessage(error));
     });
-}
-
-function getErrorMessage(error) {
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ function getErrorMessage(error) {
     if (error.code == "auth/user-not-found") {
         //serve como uma traducao
         return "Usuário nao encontrado";
@@ -27,40 +47,44 @@ function getErrorMessage(error) {
         //mas, mesmo assim, no firebase ficava cadastrado
         return "Cadastro efetuado com sucesso!";
     }
-
+ 
+ 
     return error.message;
-}
-
-function cadastrar() {
+ }
+ 
+ 
+ function cadastrar() {
     firebase.auth().createUserWithEmailAndPassword(
         //pega as informacoes do formulario e armazenam
         document.getElementById("Email").value, document.getElementById("Password").value, document.getElementById("Name").value
     ).then(response => {
         alert("Usuário cadastrado" + document.getElementById("inputEmail").value);
         console.log("Cadastrado");
-        
+       
         //isso precisa ser mudado
-        window.location.href = '/autenticacao/index.html';
-    
-    
+        window.location.href = '/Dashboard/dashboard.html';
+   
+   
     }).catch(error => {
         alert(getErrorMessage(error));
     });
-}
-
-//serve para a recuperacao de senha
-function recoverPassword() {    
+ }
+ 
+ 
+ //serve para a recuperacao de senha
+ function recoverPassword() {   
     firebase.auth().sendPasswordResetEmail(document.getElementById("Email").value).then(() => {
-        
+       
         alert('Email enviado com sucesso');
     }).catch(error => {
         hideLoading();
         alert(getErrorMessage(error));
     });
-}
-
-//serve para o logInComGoogle()
-function logInComGoogle(){
+ }
+ 
+ 
+ //serve para o logInComGoogle()
+ function logInComGoogle(){
     const provider = new firebase.auth.GoogleAuthProvider();
     //aqui eu informo o que eu estou querendo da api: as informacoes do perfil
     provider.addScope('https://www.googleapis.com/auth/userinfo.profile ');
@@ -74,11 +98,13 @@ function logInComGoogle(){
             alert("Erro ao logar! ");
             console.log(err)
         });
-} 
-
-function logout () {
+ }
+ 
+ 
+ function logout () {
     firebase.auth().signOut();
     alert("Signed Out");
     window.location.href='/index.html';
-}
-
+ }
+ 
+ 
